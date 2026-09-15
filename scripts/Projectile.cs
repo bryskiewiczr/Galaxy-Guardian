10554e1586b7a9a06aac76bad295a8aac27aa4be
bryskiewiczr <bryskiewiczr@protonmail.com>
2026-09-15 23:14:39 +0200

    feat(remove-offscreen-projectiles): remove offscreen projectiles using VisibleOnScreenNotifier2D

--- scripts/Projectile.cs
+++ scripts/Projectile.cs
@@ -3,13 +3,20 @@ using Godot;
 public partial class Projectile : Area2D {
 	[Export] private float _projectileSpeed = 500.0f;
 	private Vector2 _globalPosition;
+	private VisibleOnScreenNotifier2D _notifier2D;
 	
 	public override void _Ready() {
 		_globalPosition = GlobalPosition;
+		_notifier2D = GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D");
+		_notifier2D.ScreenExited += OnScreenExited;
 	}
 
 	public override void _PhysicsProcess(double delta) {
 		_globalPosition.X += (_projectileSpeed * (float)delta);
 		GlobalPosition = _globalPosition;
+	}
+
+	public void OnScreenExited() {
+		QueueFree();
 	}
 }
