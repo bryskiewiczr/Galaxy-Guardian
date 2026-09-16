f89745c85a42f638f1b7551ea4f80d91173b3d92
bryskiewiczr <bryskiewiczr@protonmail.com>
2026-09-17 00:18:55 +0200

    feat(difficulty-timer): add timer that increases difficulty

--- scripts/EnemySpawner.cs
+++ scripts/EnemySpawner.cs
@@ -6,23 +6,34 @@ public partial class EnemySpawner : Node2D {
     [Signal]
     public delegate void EnemySpawnedEventHandler(Enemy enemyInstance);
     
-    private Timer _timer;
+    private Timer _spawnTimer;
+    private Timer _difficultyTimer;
     private Node2D _spawnPositions;
     private Random _random;
     private PackedScene _enemyScene = GD.Load<PackedScene>("res://scenes/Enemy.tscn");
+    
+    private float _difficultyStep = 0.25f;
+    private float _maxDifficultyTimer = 0.25f;
 
     public override void _Ready() {
-        _timer = GetNode<Timer>("SpawnTimer");
+        _spawnTimer = GetNode<Timer>("SpawnTimer");
+        _difficultyTimer = GetNode<Timer>("DifficultyTimer");
         _spawnPositions = GetNode<Node2D>("SpawnPositions");
         _random = new Random();
         
-        _timer.Timeout += OnTimeoutSpawnEnemy;
+        _spawnTimer.Timeout += OnTimeoutSpawnEnemy;
+        _difficultyTimer.Timeout += OnTimeoutIncreaseDifficulty;
     }
     
     private void OnTimeoutSpawnEnemy() {
         var spawner = PickSpawner();
         var spawnerPosition = spawner.GlobalPosition;
         SpawnEnemy(position: spawnerPosition);
+    }
+
+    private void OnTimeoutIncreaseDifficulty() {
+        if (_spawnTimer.WaitTime <= _maxDifficultyTimer) return;
+        _spawnTimer.WaitTime -= _difficultyStep;
     }
 
     private Marker2D PickSpawner() {
